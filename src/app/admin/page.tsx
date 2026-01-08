44453501be4baed3dfe34d4e8d0eb0004b445779
punvit <punvit.in@gmail.com>
2026-01-08 18:14:34 +0530

    Fix: move 'use client' directive to first line in admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,4 @@
-'use client'
-    export const dynamic = 'force-dynamic'
-    "use client"
+'use client'    export const dynamic = 'force-dynamic'
 
 import { useEffect, useState } from "react"
 import { supabase } from "@/lib/supabase"
